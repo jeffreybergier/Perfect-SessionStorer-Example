3fefebe4158fb0462fe3308ae277f039d7829417
jeffreybergier <jeffburg@jeffburg.com>
2016-10-30 18:19:35 -0700

    add code for example project

--- Package.swift
+++ Package.swift
@@ -20,9 +20,10 @@
 import PackageDescription
 
 let package = Package(
-	name: "PerfectTemplate",
+	name: "Perfect-SessionStorer-Example",
 	targets: [],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0)
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2, minor: 0),
+		.Package(url: "https://github.com/jeffreybergier/Perfect-SessionStorer.git", majorVersion: 0, minor: 0)
     ]
 )
--- Sources/main.swift
+++ Sources/main.swift
@@ -24,30 +24,40 @@ import PerfectHTTPServer
 // Create HTTP server.
 let server = HTTPServer()
 
+import PerfectSessionStorer
+
+// Register the SessionStorer Filter with the servers request filters
+// This is required so that the SessionStorer can set the appropriate Cookie Tokens on every request/response
+server.setRequestFilters([(SessionInMemoryStringStorer.shared.filter, .low)])
+
 // Register your own routes and handlers
 var routes = Routes()
-routes.add(method: .get, uri: "/", handler: {
-		request, response in
-		response.setHeader(.contentType, value: "text/html")
-		response.appendBody(string: "<html><title>Hello, world!</title><body>Hello, world!</body></html>")
-		response.completed()
-	}
-)
+routes.add(method: .get, uri: "/") { request, response in
+    // Get the username for this session
+    // browse to 0.0.0.0:8181/ to see there is no username set
+    let username = SessionInMemoryStringStorer.shared[request, "username"] ?? "USER_NOT_LOGGED_IN"
+    
+    // put the username in the response
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Username = \(username)</body></html>")
+    response.completed()
+}
+routes.add(method: .get, uri: "/login") { request, response in
+    // Get the username for this session
+    // Now browse to 0.0.0.0:8181/login?username=billy
+    // to set the username to billy
+    // then browse to / to see it
+    SessionInMemoryStringStorer.shared[request, "username"] = request.param(name: "username")
+    
+    // put the username in the response
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<html><title>Hello, world!</title><body>Username Saved: \(request.param(name: "username") ?? "USERNAME_NOT_SAVED")</body></html>")
+    response.completed()
+}
 
-// Add the routes to the server.
 server.addRoutes(routes)
-
-// Set a listen port of 8181
 server.serverPort = 8181
-
-// Set a document root.
-// This is optional. If you do not want to serve static content then do not set this.
-// Setting the document root will automatically add a static file handler for the route /**
 server.documentRoot = "./webroot"
-
-// Gather command line options and further configure the server.
-// Run the server with --help to see the list of supported arguments.
-// Command line arguments will supplant any of the values set above.
 configureServer(server)
 
 do {
